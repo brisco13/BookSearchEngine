6a7defb18bfaa5f24df03d80be5c2529d8e6a505
Brisco Arechederra <brisco.arch@gmail.com>
2022-09-29 11:22:49 -0600

    added graphQL adjustments to server.js and added schema files (empty)

---
+++ README.md
@@ -0,0 +1,38 @@
+# React Portfolio 
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)    
+
+## Description
+
+This is a project that showcases my web development projects, my resume, an about me, and a contact form. This project uses the Bootstrap CSS Library and EmailJS for the contact me page. It is built using React - please reach out if you have any questions or would like to connect!  
+
+## Table of Contents
+
+[License](#license)
+
+[Features](#features)
+
+[Credits](#credits)
+
+[Contact Me](#contact-me)
+ 
+
+## License: 
+
+This project is licensed under the MIT License .
+
+---
+
+## Features: 
+This application has 4 pages including an "About Me"
+![picture1](./pics/picture1.png "About Me")
+A "Portfolio" page
+![picture1](./pics/picture3.png "Portfolio")
+A "Contact Me" page
+![picture1](./pics/picture2.png "Contact Me")
+And a "Resume" page
+![picture1](./pics/picture4.png "Resume")
+## Contact Me: 
+
+Please use the portfolio to contact me!
+ 
--- server/server.js
+++ server/server.js
@@ -1,10 +1,20 @@
 const express = require('express');
+//require Apollo Server
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+// requirements for GraphQL
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+// require for routing
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+// create server via ApolloServer that uses GraphQL
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,6 +26,19 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+  
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+  };
+  
+// Call the async function to start the server
+  startApolloServer(typeDefs, resolvers);
+ 
